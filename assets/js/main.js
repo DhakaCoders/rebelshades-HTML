28517c4e2b5da0cc179f48b6463047a667a04a48
shantoztech <45969365+shantoztech@users.noreply.github.com>
2020-03-03 18:57:42 +0600

    pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,32 @@
 var windowWidth = $(window).width();
 
 
+
+setTimeout(function() {
+    $('.countdown-popup-wrapper').css('display', 'block');
+}, 4000)
+
+$('.modal-backdrop-wrp').on('click', function(){
+	$('.countdown-popup-wrapper').slideUp();
+});
+$('.modal-close-con').on('click', function(){
+	$('.countdown-popup-wrapper').slideUp();
+});
+
+	$('#periodic-timer-countdown').syotimer({
+        hour: 0,
+        minute: 6,
+        second: 30,
+        layout: 'smhd',
+
+        headTitle: '<h3>Units of countdown in reverse order</h3>' +
+            '<p>Demonstrate layout. ' +
+            'Period is equal 2 days and 5 hours.</p>',
+        effectType: 'opacity',
+
+        periodic: false,
+        periodInterval: 53,
+        periodUnit: 'h'
+    });
+
 })(jQuery);
