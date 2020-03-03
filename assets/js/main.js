d29a6710bb22237792ab046e4d7ac48a08e9a16a
shantoztech <45969365+shantoztech@users.noreply.github.com>
2020-03-04 00:21:08 +0600

    pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,60 +3,61 @@ var windowWidth = $(window).width();
 
 
 
-// pop-up
+
 $("body").one("mouseleave", function() {
+
+    // pop-up
     $('.countdown-popup-wrapper').css('display', 'block');
-//});
 
-$('.modal-backdrop-wrp').on('click', function(){
-	$('.countdown-popup-wrapper').slideUp();
-});
-$('.modal-close-con').on('click', function(){
-	$('.countdown-popup-wrapper').slideUp();
-});
+    $('.modal-backdrop-wrp').on('click', function(){
+    	$('.countdown-popup-wrapper').slideUp();
+    });
+    $('.modal-close-con').on('click', function(){
+    	$('.countdown-popup-wrapper').slideUp();
+    });
 
-
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    'total': t,
-    'days': days,
-    'hours': hours,
-    'minutes': minutes,
-    'seconds': seconds
-  };
-}
-
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
-  var secondsSpan = clock.querySelector('.seconds');
-
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
+    // countdown
+    function getTimeRemaining(endtime) {
+      var t = Date.parse(endtime) - Date.parse(new Date());
+      var seconds = Math.floor((t / 1000) % 60);
+      var minutes = Math.floor((t / 1000 / 60) % 60);
+      var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+      var days = Math.floor(t / (1000 * 60 * 60 * 24));
+      return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+      };
     }
-  }
 
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
-}
+    function initializeClock(id, endtime) {
+      var clock = document.getElementById(id);
+      var daysSpan = clock.querySelector('.days');
+      var hoursSpan = clock.querySelector('.hours');
+      var minutesSpan = clock.querySelector('.minutes');
+      var secondsSpan = clock.querySelector('.seconds');
 
-var deadline = new Date(Date.parse(new Date()) + 15 * 01 * 04 * 09 * 1000); 
-initializeClock('clockdiv', deadline);
+      function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+          clearInterval(timeinterval);
+        }
+      }
+
+      updateClock();
+      var timeinterval = setInterval(updateClock, 1000);
+    }
+
+    var deadline = new Date(Date.parse(new Date()) + 15 * 01 * 04 * 09 * 1000); 
+    initializeClock('clockdiv', deadline);
 
 });
 
